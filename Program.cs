85df353b1455724fa80c1102c4d39dddb9257bfd
mustafa <mustafayegnidemir1@gmail.com>
2022-10-03 18:57:24 +0300

    Ders1_mysqlserver

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetVeritabani
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            GetSqlConnection();
+            GetMySqlConnection();
+        }
+
+        static void GetMySqlConnection(){
+            string connectionString = @"Data Source = .\SQLEXPRESS; Initial Catalog= Northwind2;Integrated Security=SSPI;";
+
+            string  connectionString2 = @"server=localhost;port=3306;database="
+
+            // DRIVER PROVIDER
+            using (var connection = new SqlConnection(connectionString) )
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Bağlantı Sağlandı");
+
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+
+        
+    }
+}
